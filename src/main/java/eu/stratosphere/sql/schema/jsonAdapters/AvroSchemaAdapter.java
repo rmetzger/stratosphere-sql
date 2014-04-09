bbf0eb9cf3b3e5f91d565f957f82a3cdf7f581ff
mohit <mohit.daga@ieee.org>
2014-04-09 09:34:29 +0530

    Add avro Schema

--- src/main/java/eu/stratosphere/sql/schema/jsonAdapters/AvroSchemaAdapter.java
+++ src/main/java/eu/stratosphere/sql/schema/jsonAdapters/AvroSchemaAdapter.java
@@ -1,30 +1,31 @@
 package eu.stratosphere.sql.schema.jsonAdapters;
 
 import java.io.File;
-
-import org.codehaus.jackson.JsonNode;
-
-import eu.stratosphere.sql.schema.JsonSchema.TableAdder;
+import eu.stratosphere.sql.schema.AvroStratosphereTable;
 import eu.stratosphere.sql.schema.JsonSchema;
 import eu.stratosphere.sql.schema.JsonSchemaAdapter;
 import eu.stratosphere.sql.schema.SchemaAdapterException;
+import org.apache.commons.io.FilenameUtils;
+import org.codehaus.jackson.JsonNode;
+
+import eu.stratosphere.sql.schema.JsonSchema.TableAdder;
 
 public class AvroSchemaAdapter implements JsonSchemaAdapter {
+    static {
+        JsonSchema.registerAdapter(new AvroSchemaAdapter());
+    }
 
-	static {
-		JsonSchema.registerAdapter(new AvroSchemaAdapter());
-	}
+    @Override
+    public String getTypeString() {
+        return "avro";
+    }
 
-	@Override
-	public String getTypeString() {
-		return "avro";
-	}
-
-	@Override
-	public void getTablesFromJson(JsonNode rootNode, TableAdder tableAddedImpl,
-			File file) throws SchemaAdapterException {
-		// TODO Auto-generated method stub
-
-	}
+    @Override
+    public void getTablesFromJson(JsonNode rootNode, TableAdder tableAdder, File file) throws SchemaAdapterException {
+        System.err.println("Parsing JSON schema from "+rootNode);
+        final String name = FilenameUtils.removeExtension(file.getName());
+        AvroStratosphereTable table = new AvroStratosphereTable(rootNode, name);
+        tableAdder.addTable(name, table);
+    }
 
 }
